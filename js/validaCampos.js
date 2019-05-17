a3b8a7b50589296bf55b8e5a5adad699d76961d7
Tiago Siqueira <tiagov@badesc.gov.br>
2019-05-17 16:18:26 -0300

    first commit

---
+++ js/validaCampos.js
@@ -0,0 +1,8 @@
+function validaCampo(objeto) {
+    console.log("js");
+    if(objeto.selectedIndex==0){
+        document.getElementsByClassName('txt-validacao').innerHML = "Selecione uma marca";
+        console.log("dentro do if");
+    
+    }
+}
