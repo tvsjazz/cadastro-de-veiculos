c52a433beb18273c6a320528ddbb132b5d346161
Tiago Siqueira <tiagov@badesc.gov.br>
2019-05-17 18:03:57 -0300

    começando a validar

--- js/validaCampos.js
+++ js/validaCampos.js
@@ -1,8 +1,10 @@
 function validaCampo(objeto) {
     console.log("js");
-    if(objeto.selectedIndex==0){
-        document.getElementsByClassName('txt-validacao').innerHML = "Selecione uma marca";
+    if(objeto.selectedIndex == 0){
+        var span = document.querySelector('.txt-validacao');
+        span.innerText = "Selecione uma marca";
         console.log("dentro do if");
-    
+        return false;
     }
+    return true;
 }
