fe2da600b959330e7b277818ef5882674ad3a7f3
Tiago Siqueira <tvsjazz@gmail.com>
2019-05-21 09:42:29 -0300

    arrumando script php

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
                             <label class="marca" for="marca">Marca</label>
                             <select name="marca" id="marca" required onblur="validaCampoMarca(this);">
                                 <option value="">Selecione uma marca</option>
-                                <option value="volvo">Volvo</option>
-                                <option value="volkswagen">Volkswagen</option>
-                                <option value="fiat">Fiat</option>
-                                <option value="chevrolet">Chevrolet</option>
-                                <option value="ford">Ford</option>
+                                <option value="Volvo">Volvo</option>
+                                <option value="Volkswagen">Volkswagen</option>
+                                <option value="Fiat">Fiat</option>
+                                <option value="Chevrolet">Chevrolet</option>
+                                <option value="Ford">Ford</option>
                             </select>
                             <span class="txt-valida txt-marca"></span>
                         </div>
@@ -39,41 +39,41 @@
                         <fieldset class="opcionais" onchange="validaOpcionais();">
                             <legend>Opcionais</legend>
                             <div>
-                                <input type="checkbox" class="checkbox" id="direcao-hidraulica" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="direcao-hidraulica" name="opcionais[]" value="Direção Hidráulica">
                                 <label for="direcao-hidraulica">Direção Hidraulica</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="ar-condicionado" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="ar-condicionado" name="opcionais[]" value="Ar Concicionado">
                                 <label for="ar-condicionado">Ar Condicionado</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="air-bag" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="air-bag" name="opcionais[]" value="Air Bag">
                                 <label for="air-bag">Air Bag</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="alarme" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="alarme" name="opcionais[]" value="Alarme">
                                 <label for="alarme">Alarme</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="banco-de-couro" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="banco-de-couro" name="opcionais[]" value="Banco de Couro">
                                 <label for="banco-de-couro">Banco de Couro</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="som" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="som" name="opcionais[]" value="Som">
                                 <label for="som">Som</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="travas" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="travas" name="opcionais[]" value="Travas">
                                 <label for="travas">Travas</label>
                             </div>
                             <div>
-                                <input type="checkbox" class="checkbox" id="piloto-automatico" name="opcionais[]">
+                                <input type="checkbox" class="checkbox" id="piloto-automatico" name="opcionais[]" value="Piloto Automático">
                                 <label for="piloto-automatico">Piloto Automático</label>
                             </div>
                             <div>
                                 <input type="checkbox" class="checkbox" onselect="required()" id="outro">
                                 <label for="outro">Outro</label>
-                                <input type="text" id="outro-text" name="opcionais[]" onblur="validaOutro(this);" disabled>
+                                <input type="text" id="outro-text" name="opcionais[]" onblur="validaOutro(this);" value="Outro" disabled>
                             </div>
                         </fieldset>
                         <span class="txt-valida txt-opcionais"></span>
--- script.php
+++ script.php
@@ -4,17 +4,17 @@ $marca = $_POST['marca'];
 $modelo = $_POST['modelo'];
 $anoFabricacao = $_POST['anoFabricacao'];
 
-echo "Marca: $marca";
-echo "Modelo: $modelo";
-echo "Ano de Fabricação: $anoFabricacao";
+echo "Marca: $marca<br>";
+echo "Modelo: $modelo<br>";
+echo "Ano de Fabricação: $anoFabricacao<br>";
 
-if(isset($_POST["opcionais"])) {
+if(isset($_POST['opcionais'])) {
 
     echo "Os opcionais são: <br>";
 
     // Faz loop pelo array do banco
 
-    foreach($_POST["opcionais"] as $opcional) {
+    foreach($_POST['opcionais'] as $opcional) {
 
         echo "- " . $opcional . "<br>";
     }
