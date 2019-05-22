8188c7aff4a537e910d902a880b4c3eb77ae869e
Tiago Siqueira <tvsjazz@gmail.com>
2019-05-22 10:18:27 -0300

    conexao mysql

--- script.php
+++ script.php
@@ -4,6 +4,16 @@
 $marca = $_POST['marca'];
 $modelo = $_POST['modelo'];
 $anoFabricacao = $_POST['anoFabricacao'];
+$direcao = "";
+$arCondicionado = "";
+$airBag = "";
+$alarme = "";
+$bancoDeCouro = "";
+$som = "";
+$travas = "";
+$pilotoAutomatico = "";
+$outro = "";
+
 
 if(isset($_POST['opcionais'])) {
 
@@ -11,11 +21,20 @@ if(isset($_POST['opcionais'])) {
     foreach($_POST['opcionais'] as $opcional) {
 
         $listaOpcionais =  "- $opcional <br>";
+        if($opcional == "Direção Hidráulica") {
+            echo $opcional;
+            $direcao = $opcional;
+        }
+        if($opcional == "Ar Concicionado") {
+            echo $opcional;
+            $arCondicionado = $opcional;
+        }
     }
 }
-$conexao = mysqli_connect('localhost', 'root', '', 'locadora');
 
-$query = "insert into veiculos (marca, modelo, anoFabricacao, opcionais) values ( '{$marca}', '{$modelo}', '{$anoFabricacao}', '{$listaOpcionais}'";
+$conexao = mysqli_connect('localhost', 'root', '', 'locadoraveiculos');
+
+$query = "insert into veiculos (marca, modelo, ano, direcao,ar_condicionado, air_bag, alarme, banco_de_couro, som, travas, piloto_automatico, outro) values ( '{$marca}', '{$modelo}', '{$anoFabricacao}', '{$direcao}', '{$arCondicionado}', '{$airBag}', '{$alarme}', '{$bancoDeCouro}', '{$som}', '{$travas}', '{$pilotoAutomatico}', '{$outro}'";
 if(mysqli_query($conexão, $query)) { ?>
     <p class="alert-success">O carro da marca <?= $marca ?>, modelo <?= $modelo ?>, ano de fabricação <?= $anoFabricacao ?> <br>
                             e com os seguintes opcionais: <?= $listaOpcionais ?> </p>
